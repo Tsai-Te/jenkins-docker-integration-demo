8eecccf7b8b7b1f3b5d2b4feecbad5774c0bb4df
Te <tsai.te0210@gmail.com>
2023-08-10 15:03:25 -0400

    first commit

---
+++ src/main/java/com/jenkinsdockerdemo/jenkinsdockerintegrationdemo/JenkinsDockerIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsdockerdemo.jenkinsdockerintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsdockerdemo/jenkinsdockerintegrationdemo/JenkinsDockerIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsdockerdemo.jenkinsdockerintegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
