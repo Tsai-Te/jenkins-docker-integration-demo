51f8e9eb2424e501183d3aa1d5107cc4ab7da730
Te <tsai.te0210@gmail.com>
2023-08-10 15:33:34 -0400

    First Get method

--- src/main/java/com/jenkinsdockerdemo/jenkinsdockerintegrationdemo/JenkinsDockerIntegrationDemoApplication.java
+++ src/main/java/com/jenkinsdockerdemo/jenkinsdockerintegrationdemo/JenkinsDockerIntegrationDemoApplication.java
@@ -2,10 +2,17 @@ package com.jenkinsdockerdemo.jenkinsdockerintegrationdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDockerIntegrationDemoApplication {
 
+	@GetMapping
+	public String hello() {
+		return "Hello, welcome...";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerIntegrationDemoApplication.class, args);
 	}
